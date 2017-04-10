3647309abe3d0eb7d7e0feb62bd9b6db8418fe6a
liangwei0101 <liangwei0101@outlook.com>
2017-04-10 11:28:58 +0800

    测试

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,194 +1,103 @@
 <template>
   <div class="hello">
-  <p>我是测试</p>
+    <button class="btn btn-success" @click="addDialogShow"><i class="fa fa-bars"></i> 新 增</button>
+    <!-- START MODAL ICON PREVIEW -->
+    <div :class="isAbleShowClass" id="iconPreview" role="dialog" tabindex="-1"  aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" @click="addDialogClose"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title">我是标题</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="icon-preview"></div>
+              </div>
+              <div class="col-md-12">
+                <form class="form-horizontal" role="form">
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Text</label>
+                    <div class="col-md-10">
+                      <input type="text" class="form-control" value="Some text value..."/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Password</label>
+                    <div class="col-md-10">
+                      <input type="password" class="form-control" value="password"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Readonly</label>
+                    <div class="col-md-10">
+                      <input type="text" class="form-control" readonly value="Readonly value"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Disabled</label>
+                    <div class="col-md-10">
+                      <input type="text" class="form-control" readonly value="Disabled value"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Placeholder</label>
+                    <div class="col-md-10">
+                      <input type="text" class="form-control" placeholder="Fill this field please"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-md-2 control-label">Text area</label>
+                    <div class="col-md-10">
+                      <textarea class="form-control" rows="5"></textarea>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="addDialogClose">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END MODAL ICON PREVIEW -->
   </div>
 </template>
 
 <script>
-  import Pagination from './Pagination.vue'
   import Vue from 'vue'
   import VueResource from 'vue-resource'
   import { getCookie } from '../services/Cookie'
   Vue.use(VueResource)
 
   export default {
-    components: { Pagination },
-    data () {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'))
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'))
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'))
-            } else {
-              callback()
-            }
-          }
-        }, 1000)
-      }
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'))
-        } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass')
-          }
-          callback()
-        }
-      }
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'))
-        } else if (value !== this.ruleForm2.pass) {
-          callback(new Error('两次输入密码不一致!'))
-        } else {
-          callback()
-        }
-      }
-      var validUerid = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('请输入用户编号'))
-        }
-        callback()
-      }
-      var validName = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('请输入用户名称'))
-        }
-        callback()
-      }
-      var validPhone = (rule, value, callback) => {
-        if (value === '') {
-          return callback(new Error('请输入手机号码'))
-        } else {
-          var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/
-          if (!myreg.test(value)) {
-            return callback(new Error('请输入有效的手机号码'))
-          }
-        }
-        callback()
-      }
+    data: function () {
       return {
-        usersShow: [],
-        users: [],
-        delUser: '',
-        selectText: '',
-        freezeUserInfo: '',
-        addFormVisible: true,
-        addLoading: false,
-        editFormVisible: false,
-        editLoading: false,
-        delClass: {
-          open: false
-        },
-        freezeClass: {
-          open: false
-        },
-        ruleForm2: {
-          userid: '',
-          name: '',
-          pass: '',
-          checkPass: '',
-          age: '',
-          email: '',
-          telephone: ''
-        },
-        editForm: {
-          userid: '',
-          name: '',
-          pass: '',
-          checkPass: '',
-          age: '',
-          email: '',
-          telephone: ''
-        },
-        rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
-          ],
-          userid: [
-            {validator: validUerid, trigger: 'blur'}
-          ],
-          name: [
-            {validator: validName, trigger: 'blur'}
-          ],
-          telephone: [
-            {validator: validPhone, trigger: 'blur'}
-          ]
-        },
-        apiUrl: 'http://localhost:3000/api',
-        //  分页控件相关属性
-        pageOne: {
-          currentPage: 1,
-          totalPages: 10,
-          totalItems: 0,
-          itemsPerPage: 10
-        },
-        //  默认10条每页
-        selected: '10',
-        options: [
-          { text: '10条/页', value: '10' },
-          { text: '20条/页', value: '20' },
-          { text: '50条/页', value: '50' },
-          { text: '100条/页', value: '100' }
-        ]
+        isAbleShowClass: {
+          modal: true,
+          fade: true
+        }
       }
     },
     mounted: function () {
-      this.getOne()
+      this.vueCookie()
     },
     methods: {
-      //  显示新增界面
-      handleAdd: function () {
-        this.addFormVisible = true
-        this.ruleForm2 = {
-          userid: '',
-          name: '',
-          pass: '',
-          checkPass: '',
-          age: '',
-          email: '',
-          telephone: ''
-        }
+      addDialogShow: function () {
+        this.isAbleShowClass.modal = false
+        this.isAbleShowClass.fade = false
       },
-      //  显示编辑界面
-      handleEdit: function (user) {
-        this.editFormVisible = true
-        this.editForm = {
-          userid: user.userid,
-          name: user.name,
-          pass: user.password,
-          email: user.email,
-          telephone: user.telephone,
-          isAble: user.isAble
-        }
+      addDialogClose: function () {
+        this.isAbleShowClass.modal = true
+        this.isAbleShowClass.fade = true
       },
-      submitForm (formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.createUser()
-            this.addFormVisible = false
-          } else {
-            console.log('error submit!!')
-            return false
-          }
-        })
-      },
-      getOne: function () {
-        var Dic = getCookie()
-        console.log('--------------------')
-        console.log(Dic['userid'])
-        console.log('--------------------')
+      vueCookie: function () {
+        this.Dic = getCookie()
+        console.log('-------------------------')
+        console.log(this.Dic['userid'])
+        console.log('-------------------------')
       }
     }
   }
