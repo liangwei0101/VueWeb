27583828f6ab5da1d82f24792ddb1c5c8f9513b6
liangwei0101 <liangwei0101@outlook.com>
2017-04-10 11:26:32 +0800

    链接的改变

--- src/components/PublicPage/PageSidebar.vue
+++ src/components/PublicPage/PageSidebar.vue
@@ -28,59 +28,9 @@
       <li>
         <a href="index.html"><span class="fa fa-desktop"></span> <span class="xn-text">主页</span></a>
       </li>
-      <li class="xn-openable">
-        <a href="#"><span class="fa  fa-stack-overflow"></span> <span class="xn-text">放射源信息管理</span></a>
-        <ul>
-          <li><a href="pages-gallery.html"><span class="fa fa-image"></span> Gallery</a></li>
-          <li><a href="pages-profile.html"><span class="fa fa-user"></span> Profile</a></li>
-          <li><a href="pages-address-book.html"><span class="fa fa-users"></span> Address Book</a></li>
-          <li class="xn-openable">
-            <a href="#"><span class="fa fa-clock-o"></span> Timeline</a>
-            <ul>
-              <li><a href="pages-timeline.html"><span class="fa fa-align-center"></span> Default</a></li>
-              <li><a href="pages-timeline-simple.html"><span class="fa fa-align-justify"></span> Full Width</a></li>
-            </ul>
-          </li>
-          <li class="xn-openable">
-            <a href="#"><span class="fa fa-envelope"></span> Mailbox</a>
-            <ul>
-              <li><a href="pages-mailbox-inbox.html"><span class="fa fa-inbox"></span> Inbox</a></li>
-              <li><a href="pages-mailbox-message.html"><span class="fa fa-file-text"></span> Message</a></li>
-              <li><a href="pages-mailbox-compose.html"><span class="fa fa-pencil"></span> Compose</a></li>
-            </ul>
-          </li>
-          <li><a href="pages-messages.html"><span class="fa fa-comments"></span> Messages</a></li>
-          <li><a href="pages-calendar.html"><span class="fa fa-calendar"></span> Calendar</a></li>
-          <li><a href="pages-tasks.html"><span class="fa fa-edit"></span> Tasks</a></li>
-          <li><a href="pages-content-table.html"><span class="fa fa-columns"></span> Content Table</a></li>
-          <li><a href="pages-faq.html"><span class="fa fa-question-circle"></span> FAQ</a></li>
-          <li><a href="pages-search.html"><span class="fa fa-search"></span> Search</a></li>
-          <li class="xn-openable">
-            <a href="#"><span class="fa fa-file"></span> Blog</a>
-
-            <ul>
-              <li><a href="pages-blog-list.html"><span class="fa fa-copy"></span> List of Posts</a></li>
-              <li><a href="pages-blog-post.html"><span class="fa fa-file-o"></span>Single Post</a></li>
-            </ul>
-          </li>
-          <li class="xn-openable">
-            <a href="#"><span class="fa fa-sign-in"></span> Login</a>
-            <ul>
-              <li><a href="pages-login.html">App Login</a></li>
-              <li><a href="pages-login-website.html">Website Login</a></li>
-              <li><a href="pages-login-website-light.html"> Website Login Light</a></li>
-            </ul>
-          </li>
-          <li class="xn-openable">
-            <a href="#"><span class="fa fa-warning"></span> Error Pages</a>
-            <ul>
-              <li><a href="pages-error-404.html">Error 404 Sample 1</a></li>
-              <li><a href="pages-error-404-2.html">Error 404 Sample 2</a></li>
-              <li><a href="pages-error-500.html"> Error 500</a></li>
-            </ul>
-          </li>
-        </ul>
-      </li>
+      <router-link to="/RadioactiveInfo">
+        <li><a><span class="fa fa-stack-overflow"></span> <span class="xn-text"> 放射源信息管理</span></a></li>
+      </router-link>
       <router-link to="/UserManage">
         <li><a><span class="fa fa-cog"></span> <span class="xn-text"> 用户管理</span></a></li>
       </router-link>
@@ -107,27 +57,9 @@
       <li><a><span class="fa fa-envelope"></span> <span class="xn-text"> 邮件样式设置</span></a></li>
       </router-link>
 
-      <li class="xn-openable">
-        <a href="#"><span class="fa fa-sitemap"></span> <span class="xn-text">Navigation Levels</span></a>
-        <ul>
-          <li class="xn-openable">
-            <a href="#">Second Level</a>
-            <ul>
-              <li class="xn-openable">
-                <a href="#">Third Level</a>
-                <ul>
-                  <li class="xn-openable">
-                    <a href="#">Fourth Level</a>
-                    <ul>
-                      <li><a href="#">Fifth Level</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
+      <router-link to="/test">
+        <li><a><span class="fa fa-envelope"></span> <span class="xn-text"> 测试</span></a></li>
+      </router-link>
     </ul>
     <!-- END X-NAVIGATION -->
   </div>
